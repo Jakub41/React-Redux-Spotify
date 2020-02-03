33a3252b4a5d540843f422e7f1cffd8a90bcad5e
jakub41 <lemiszewski@gmx.com>
2020-02-03 16:17:21 +0100

    Added missed library reduser

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,11 @@
 import { combineReducers, createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import artistsReducer from "./Reducers/artistsReducer";
+import libraryReducer from "./Reducers/LibraryReducer";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 
-const rootReducer = combineReducers({ artistsReducer });
+const rootReducer = combineReducers({ artistsReducer, libraryReducer });
 function configureStore() {
   return createStore(
     rootReducer,
